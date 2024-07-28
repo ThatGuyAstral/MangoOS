c761ecedee6b2cd2a5d2eec69c49d97392037c6a
cosmic. <144744086+ThatGuyAstral@users.noreply.github.com>
2024-07-28 00:59:32 -0400

    Delete kernel/src/mangoshell directory

--- kernel/src/mangoshell/commands/hello.c
+++
@@ -1,6 +0,0 @@
-#include <mangoshell/commands/commands.h>
-
-void hello(struct flanterm_context *ftctx/*int argc, char** argv*/)
-{
-    // NO printf(ftctx, "Hello!");
-}
--- kernel/src/mangoshell/mangoshell.c
+++
@@ -1,6 +0,0 @@
-#include <mangoshell/mangoshell.h>
-
-void mshMain(struct flanterm_context *ftctx)
-{
-    // no printf(ftctx, "hello, world!\n");
-}
