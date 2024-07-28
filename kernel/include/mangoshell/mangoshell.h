a4e8edf24fdf60e24645ae8c947c0819522c4220
cosmic. <144744086+ThatGuyAstral@users.noreply.github.com>
2024-07-28 00:59:50 -0400

    Delete kernel/include/mangoshell directory

--- kernel/include/mangoshell/mangoshell.h
+++
@@ -1,8 +0,0 @@
-#ifndef MANGOSHELL_H
-#define MANGOSHELL_H
-#include <flanterm/flanterm.h>
-
-void mshMain(struct flanterm_context *ftctx);
-void mshLog(struct flanterm_context *ftctx, int type, const char *msg);
-
-#endif
